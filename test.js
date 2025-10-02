2861f2a863f08624d8e38197742121e004db257b
Anna <ledy-zaharova-03@ukr.net>
2025-10-02 21:40:23 +0300

    Add GitHub Actions geom1

--- test.js
+++ test.js
@@ -12,6 +12,5 @@ function assertEqual(actual, expected, message) {
 // Тести середнього геометричного
 assertEqual(geometricMean([1, 1, 1]), 1, 'Середнє геометричне [1,1,1] = 1');
 assertEqual(geometricMean([2, 8]), 4, 'Середнє геометричне [2,8] = 4');
-assertEqual(geometricMean([1, 3, 9, 27]), 6.4807, 'Середнє геометричне [1,3,9,27] = 6.4807');
-
+assertEqual(geometricMean([1,3,9,27]), 6.062, 'Середнє геометричне [1,3,9,27] ≈ 6.062');
 console.log("Усі тести пройдено!");
